1244582b0fb2300f5c2ef1c5132105817306dd49
Jax Tan <niftyjax@gmail.com>
2024-11-14 13:03:35 -0800

    Now the background color is implemented

--- services/aiPhotoService.js
+++ services/aiPhotoService.js
@@ -8,78 +8,117 @@ const AI_SERVICE_URL = process.env.AI_SERVICE_URL;
 const AI_SERVICE_PORT = process.env.AI_SERVICE_PORT;
 
 export async function processIdPhoto(image, params) {
-  try {
-    const formData = new FormData();
-
-    // Ensure we're sending the buffer correctly
-    if (!image.buffer) {
-        throw new Error('No image buffer provided');
-    }
-    
-    // Append the image buffer with proper metadata
-    formData.append('input_image', image.buffer, {
-        filename: image.originalname,
-        contentType: image.mimetype,
-    });
-
     // Rename keys in params if they exist
     const renamedParams = {
-      ...params,
-      render: params.renderMode,
-      color: params.backgroundColor
+        ...params,
+        render: params.renderMode,
+        color: params.backgroundColor
     };
     // Remove old keys
     delete renamedParams.renderMode;
     delete renamedParams.backgroundColor;
 
+    const image_no_background_base64_hd = await processCallIdphoto(image, renamedParams);
+    return await processCallAddBackground(image_no_background_base64_hd, renamedParams);
+}
+
+async function processCallIdphoto(image, params) {
+    try {
+        const formData = new FormData();
+    
+        // Ensure we're sending the buffer correctly
+        if (!image.buffer) {
+            throw new Error('No image buffer provided');
+        }
+        
+        // Append the image buffer with proper metadata
+        formData.append('input_image', image.buffer, {
+            filename: image.originalname,
+            contentType: image.mimetype,
+        });
+    
+        const defaultParams = {
+          head_measure_ratio: 0.2,
+          head_height_ratio: 0.45,
+          top_distance_max: 0.12,
+          top_distance_min: 0.1,
+          height: 413,
+          width: 295,
+          human_matting_model: 'modnet_photographic_portrait_matting',
+          face_detect_model: 'mtcnn',
+          hd: 'true',
+          dpi: 300,
+        };
+    
+        const finalParams = { ...defaultParams, ...params };
+    
+        const requiredParams = [
+            'height',
+            'width',
+            'human_matting_model',
+            'face_detect_model',
+            'hd',
+            'dpi',
+            'head_measure_ratio',
+            'head_height_ratio',
+            'top_distance_max',
+            'top_distance_min',
+            ];
+    
+        Object.entries(finalParams)
+            .filter(([key]) => requiredParams.includes(key))
+            .forEach(([key, value]) => {
+                formData.append(key, value);
+            });
+    
+    
+        const response = await axios({
+          method: 'post',
+          url: `http://${AI_SERVICE_URL}:${AI_SERVICE_PORT}/idphoto`,
+          data: formData,
+          headers: {
+            ...formData.getHeaders(),
+          },
+        });
+    
+        return response.data.image_base64_hd;
+      } catch (error) {
+        console.error('Error processing ID photo:', error);
+        throw error;
+      }
+}
+
+async function processCallAddBackground(image_no_background_base64_hd, params) {
+    const formData = new FormData();
+
+    formData.append('input_image_base64', image_no_background_base64_hd);
+
     const defaultParams = {
-      head_measure_ratio: 0.2,
-      head_height_ratio: 0.45,
-      top_distance_max: 0.12,
-      top_distance_min: 0.1,
-      height: 413,
-      width: 295,
-      human_matting_model: 'modnet_photographic_portrait_matting',
-      face_detect_model: 'mtcnn',
-      hd: 'true',
-      dpi: 300,
+        dpi: 300,
     };
 
     const finalParams = { ...defaultParams, ...params };
 
     const requiredParams = [
-        'input_image',
-        'height',
-        'width',
-        'human_matting_model',
-        'face_detect_model',
-        'hd',
+        'color',
         'dpi',
-        'head_measure_ratio',
-        'head_height_ratio',
-        'top_distance_max',
-        'top_distance_min',
-        ];
+        'render',
+    ];
 
     Object.entries(finalParams)
         .filter(([key]) => requiredParams.includes(key))
         .forEach(([key, value]) => {
-            formData.append(key, value);
-        });
-
-
-    const response = await axios({
-      method: 'post',
-      url: `http://${AI_SERVICE_URL}:${AI_SERVICE_PORT}/idphoto`,
-      data: formData,
-      headers: {
-        ...formData.getHeaders(),
-      },
+        formData.append(key, value);
     });
 
-    return response.data;
-  } catch (error) {
-    console.error('Error processing ID photo:', error);
-    throw error;
-  }
+    const response = await axios({
+        method: 'post',
+        url: `http://${AI_SERVICE_URL}:${AI_SERVICE_PORT}/add_background`,
+        data: formData,
+        headers: {
+          ...formData.getHeaders(),
+        },
+      });
+  
+      return response.data
 }
